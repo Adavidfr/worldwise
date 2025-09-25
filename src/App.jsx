632999d6c0972988231ce334019d8255c1359ed4
Alex David Frías <adavidfruiz@gmail.com>
2025-09-24 20:49:31 -0500

    feat: implement routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
-import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Homepage />}
+        />
+        <Route
+          path="/product"
+          element={<Product />}
+        />
+        <Route
+          path="/pricing"
+          element={<Pricing />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="app"
+          element={<AppLayout />}
+        />
+        <Route
+          path="*"
+          element={<PageNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
